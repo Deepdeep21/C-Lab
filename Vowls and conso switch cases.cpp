ca42f3dcc3bb728d09176c3f301ce85c1b3be18e
Deepdeep21 <96103257+Deepdeep21@users.noreply.github.com>
2021-12-14 11:35:55 +0530

    Add files via upload

---
+++ Vowls and conso switch cases.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+main()
+{
+	char a;
+	printf("enter a character");
+	scanf("%c",&a);
+	switch(a)
+	{
+	case 'a':
+    case 'e':
+    case 'i':
+	case 'o':
+	case 'u':
+	case 'A':
+    case 'E':
+    case 'I':
+	case 'O':
+	case 'U':
+	printf("VOWl");
+	break;
+    default:	printf("Consonant");
+
+	}
+			
+}
